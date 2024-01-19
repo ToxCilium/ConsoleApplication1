beea327042155aa274e331345dcac648f0c1c05b
Cilum <sereda_maksimka@bk.ru>
2024-01-19 14:09:56 +0300

    Исправлены ошибки в Process

--- ConsoleApplication1/process.cpp
+++ ConsoleApplication1/process.cpp
@@ -7,7 +7,7 @@ using namespace std;
 float process(catalog* array[], int size, char category[MAX_STRING_SIZE])
 {
 	float max = 0;
-	for (int i = 1; i < size; i++)
+	for (int i = 0; i < size; i++)
 	{
 		if (strcmp(array[i]->category,category)==0)
 		{
